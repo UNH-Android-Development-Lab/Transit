43e97a34a7ab88de06c868574faf095319206ede
Ryan McCann <ryan@ryan-mccann.com>
2015-12-02 11:21:38 -0500

    MainActivity with tabs and ViewPager. Includes colors.xml and updated styles.xml. Addresses #4

--- mobile/src/main/java/edu/unh/cs/android/transit/myapplication/MainActivity.java
+++ mobile/src/main/java/edu/unh/cs/android/transit/myapplication/MainActivity.java
@@ -1,9 +1,18 @@
 package edu.unh.cs.android.transit.myapplication;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.main_viewPager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MapFragment(), getString(R.string.tab_nearby));
+        adapter.addFragment(new SearchFragment(), getString(R.string.tab_search));
+        adapter.addFragment(new BookmarksFragment(), getString(R.string.tab_saved_stops));
+        viewPager.setAdapter(adapter);
     }
 
 
@@ -34,5 +61,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragments = new ArrayList<Fragment>();
+        private final List<String> fragmentTitles = new ArrayList<String>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragments.add(fragment);
+            fragmentTitles.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitles.get(position);
+        }
     }
 }
